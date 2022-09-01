9a3785696f207ac74723e15a6b7264f930e56a7c
Ryan Bennett <your@mail.com>
2022-08-31 19:14:38 -0600

    remove dead code

--- rollup.config.js
+++ rollup.config.js
@@ -5,34 +5,7 @@ import globals from "rollup-plugin-node-globals";
 import replace from "@rollup/plugin-replace";
 
 export default [
-  {
-    input: "src/fetch.inject.js",
-    output: {
-      dir: "dist",
-      format: "iife",
-    },
-    plugins: [
-      commonjs(),
-      resolve({ preferBuiltins: false }),
-      globals(),
-      builtins({}),
-      replace({
-        delimiters: ["", ""],
-        "crypto_1.default.randomBytes(4)":
-          "window.crypto.getRandomValues(new Uint8Array(4))",
-        "crypto_1.default.randomBytes(8)":
-          "window.crypto.getRandomValues(new Uint8Array(8))",
-      }),
-    ],
-  },
-  {
-    input: "src/fetch.expo.js",
-    output: {
-      dir: "dist",
-      format: "cjs",
-    },
-    plugins: [commonjs(), resolve(), globals(), builtins({ crypto: true })],
-  },
+
   {
     input: "src/pocket_proxy.js",
     output: {
@@ -63,13 +36,5 @@ export default [
           "function checkTypeSupport(type) { return false;",
       }),
     ],
-  },
-  {
-    input: "src/storage.expo.js",
-    output: {
-      dir: "dist",
-      format: "cjs",
-    },
-    plugins: [commonjs(), resolve(), globals(), builtins({ crypto: true })],
-  },
+  }
 ];
